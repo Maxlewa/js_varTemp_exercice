e85deee8c02f6d61350c7c6f5acf1cc24eb70b48
Maxence <lemairemaxence@hotmail.com>
2021-01-05 10:46:33 +0100

    exo temp ok

---
+++ public/js/main.js
@@ -0,0 +1,97 @@
+// 2, 3, 1
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let temp;
+
+// temp = a
+// a = b
+// b = c
+// c = temp
+
+// console.log(a, b, c);
+
+// EXO 1 - 4, 3, 2, 1
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4; 
+// let temp;
+
+// temp = d;
+// d = a;
+// a = temp;
+
+// temp = c;
+// c = b;
+// b = temp;
+
+// console.log(a, b, c, d);
+
+// EXO 2 - 2, 3, 4, 1
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// temp = a
+// a = b
+// b = c
+// c = d
+// d = temp
+
+// console.log(a, b, c, d);
+
+// EXO 3 - 4, 3, 2, 1
+
+// let tableau = [1, 2, 3, 4]
+
+// let temp;
+// temp = tableau[0]
+
+// tableau[0] = tableau[3]
+// tableau[3] = temp
+// temp = tableau[1]
+// tableau[1] = tableau[2]
+// tableau[2] = temp
+
+// console.log(tableau);
+
+// EXO 4 - 4, 5, 1, 2, 6, 3
+
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+let e = 5
+let f = 6
+
+// let temp;
+// let tempDeux;
+
+// temp = c
+// c = a
+// tempDeux = f
+// f = temp
+// temp = e
+// e = tempDeux
+// tempDeux = b
+// b = temp
+// temp = d
+// d = tempDeux
+// a = temp
+
+temp = c
+
+c = a 
+a = d
+d = b 
+b = e
+e = f
+f = temp
+
+console.log(a, b, c, d, e, f);
